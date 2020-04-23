f8bdedde283ae639e6bda12614db44a266e972c9
BlackFlair <surinmachaiah@gmail.com>
2020-04-23 09:40:37 +0530

    ReadData.py

---
+++ ReadData.py
@@ -0,0 +1,7 @@
+# To get details about the camera position and the camera network of the area.
+
+import pandas as pd
+import numpy as np
+
+camDetails = pd.read_csv('') # Should contain Camera ID and Camera Location
+camConnections = pd.read_csv('') # Should contain Primary Camera and all the connecting Secondary Cameras
